0e7dcb2f2b01285a65442da85a8ccfd518bcc96e
Cold Stone <xiaoranran1993@outlook.com>
2019-11-27 15:05:41 +0800

    test: oss controller test

--- test/app/controller/oss.test.ts
+++ test/app/controller/oss.test.ts
@@ -1,17 +1,36 @@
-import { app } from 'egg-mock/bootstrap'
+import { app, assert } from 'egg-mock/bootstrap'
 
 describe('测试 oss controller', () => {
-  it('请求数据类型不是 form-data 返回错误', async () => {
+  it('请求头数据类型不是 form-data 返回错误', async () => {
     return app.httpRequest()
       .post('/upload-server/v1/oss')
-      .expect(500)
+      .expect(400)
   })
 
-  it('请求需要带上文件, 否则返回错误', async () => {
+  it('请求需要有上传文件, 否则返回错误', async () => {
     return app.httpRequest()
       .post('/upload-server/v1/oss')
-      .type('form')
-      .expect(500)
+      .set('Content-type', 'multipart/form-data; boundary=--------------------------820317025826519383208825')
+      .expect(400)
+  })
+
+  it('上传文件需要设置 field', async () => {
+    return app.httpRequest()
+      .post('/upload-server/v1/oss')
+      .set('Content-type', 'multipart/form-data; boundary=--------------------------820317025826519383208825')
+      .attach('', 'test/app/controller/avatar.png')
+      .expect(400)
+  })
+
+  it('上传文件成功', async () => {
+    const result = await app.httpRequest()
+      .post('/upload-server/v1/oss')
+      .set('Content-type', 'multipart/form-data; boundary=--------------------------820317025826519383208825')
+      .field('folder', 'images')
+      .attach('avatar', 'test/app/controller/avatar.png')
+      .expect(200)
+
+    assert(result.body)
   })
 
 })
