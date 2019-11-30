eaeb54e2947ec4f4cc37a3ae64c2a06f6fd519e4
Cold Stone <xiaoranran1993@outlook.com>
2019-11-30 10:47:07 +0800

    refactor: no rate limit

--- app/router.ts
+++ app/router.ts
@@ -3,5 +3,6 @@ import { Application } from 'egg'
 export default (app: Application) => {
   const { controller, router } = app
 
-  router.post(`/${app.name}/v1/upload`, app.middleware.ratelimit(), controller.oss.upload)
+  // router.post(`/${app.name}/v1/upload`, app.middleware.ratelimit(), controller.oss.upload)
+  router.post(`/${app.name}/v1/upload`, controller.oss.upload)
 };
